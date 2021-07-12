32b5b7a102db496314b3dcd8650cf5feb519d7c6
chichier <78356244+chichier@users.noreply.github.com>
2021-07-12 19:46:00 +0800

    20210712

---
+++ component/Distinguish_counter/sim/tb_Distinguish_counter.v
@@ -0,0 +1,45 @@
+`timescale 1ns/1ps
+module tb_Distinguish_counter;
+    
+reg clk,cmp,set;
+wire [3:0] sel;
+Distinguish_counter u0(
+        cmp,
+        clk,
+        set,
+        sel
+);
+initial begin
+    clk=1'b0;cmp=0;set=0;
+forever begin
+    #500 clk=~clk;
+end
+end
+
+initial begin
+    #4000000 $stop;
+end
+initial begin
+    #110000 cmp=1;#8 cmp=0;
+end
+initial begin
+    #1390000 cmp=1;#8 cmp=0;    
+end
+
+initial begin
+    #2499000 cmp=1;#8 cmp=0;
+end
+
+initial begin
+    #3700000 cmp=1;#8 cmp=0;
+end
+initial begin
+    #999500 set=1;#500 set=0;
+     #999500 set=1;#500 set=0;
+      #999500 set=1;#500 set=0;
+       #999500 set=1;#500 set=0;
+       
+    
+   
+end
+endmodule
